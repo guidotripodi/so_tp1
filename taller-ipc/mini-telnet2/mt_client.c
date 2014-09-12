eef8720f66cceedab315ebac7127c65f04009c65
guidotripodi <guido.tripodi@hotmail.com>
2014-09-11 21:02:24 -0300

    arribaaa!

--- taller-ipc/mini-telnet2/mt_client.c
+++ taller-ipc/mini-telnet2/mt_client.c
@@ -9,8 +9,11 @@ int main(int argc, char* argv[]) {
 
 	/* Crear socket sobre el que se lee: dominio INET, protocolo UDP (DGRAM). */
 	sock = socket(AF_INET, SOCK_STREAM, 0);
-
-	if (inet_aton(argv[1], &inp) != 0) {
+	if(sock < 0){
+		perror("Error al crear el socket");
+		exit(1);
+	}
+	if (inet_aton(argv[1], (struct in_addr*) &inp) != 0) {
 		name.sin_family = AF_INET;
 		name.sin_addr = inp;
 		name.sin_port = htons(PORT);
@@ -50,22 +53,11 @@ int main(int argc, char* argv[]) {
 
 	    }
 
-		/*while (fgets(input, MAX_MSG_LENGTH, stdin)) {
-		    if (send(sock, input, strlen(input), 0) == -1) {
-		        perror("enviando");
-		        exit(1);
-		    }
-			
-			read(sock, output, MAX_MSG_LENGTH);
-			printf("RESPUESTA SERVIDOR %s", output);
-
-			if (strncmp(input, END_STRING, MAX_MSG_LENGTH) == 0) {
-				close(sock);
-				break;
-			}
-		}*/
+	
 
 	}
 
 	return 0;
 }
+
+
