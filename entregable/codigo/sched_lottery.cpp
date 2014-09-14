db36da2a69cf47ef7f65d8863e06c681e5a6b464
agustin.rodriguez <agustin.rodriguez@etermax.com>
2014-09-14 17:24:50 -0300

    Idea basica del lottery implementada. Tira segm fault

--- entregable/codigo/sched_lottery.cpp
+++ entregable/codigo/sched_lottery.cpp
@@ -1,24 +1,115 @@
 #include "sched_lottery.h"
+#include <stdlib.h>
 
 using namespace std;
 
+const int QUANTUM_TOTAL = 10;
+
 SchedLottery::SchedLottery(vector<int> argn) {
   // FCFS recibe la cantidad de cores.
+
+	for (int i = 0; i < argn[0]; i++){
+		quantumActual.push_back(QUANTUM_TOTAL);
+	}
+
+	srand(argn[1]);
 }
 
 SchedLottery::~SchedLottery() {
 }
 
 void SchedLottery::load(int pid) {
-  load(pid,0);
+  load(pid, 0);
 }
 
-void SchedLottery::load(int pid,int deadline) {
+void SchedLottery::load(int pid, int deadline) {
+	// Lo agrego a la lista de procesos
+	process.push_back(pid);
+	int nuevoTotal = process.size();
+
+	tickets = QUANTUM_TOTAL * nuevoTotal * 1000;
+	int ticketsProceso = tickets/nuevoTotal;
+
+	processTickets.push_back(ticketsProceso);
+
+	for (int i = 0; i < nuevoTotal; i++) {
+		processTickets[i] = ticketsProceso;
+	}
 }
 
 void SchedLottery::unblock(int pid) {
+	// Por ahora lo vuelvo a cargar de nuevo como un proceso nuevo
+	// Se le asignara la cantidad de tickets proporcional
+	load(pid);
 }
 
 int SchedLottery::tick(int cpu, const enum Motivo m) {
-  return 0;
+	switch (m) {
+		case EXIT:
+			sacar(current_pid(cpu));
+			return next(cpu);
+			break;
+		case BLOCK:
+			sacar(current_pid(cpu));
+			return next(cpu);
+			break;
+		case TICK:
+			if (current_pid(cpu) == IDLE_TASK) {
+				if (!process.empty()) {
+					return next(cpu);
+				} else {
+					return IDLE_TASK;
+				}
+			} else {
+				quantumActual[cpu]--;
+				if (quantumActual[cpu] == 0) {
+					load(current_pid(cpu));
+					return next(cpu);
+				} else {
+					return current_pid(cpu);
+				}
+			}
+			break;
+	}
+}
+
+void SchedLottery::sacar(int pid) {
+	int length = process.size(), i;
+
+	for (i = 0; i < length; i++) {
+		if (process[i] == pid) {
+			break;
+		}
+	}
+
+	process.erase(process.begin() + i);
+	processTickets.erase(processTickets.begin() + i);
+
+	int nuevoTotal = process.size();
+	tickets = QUANTUM_TOTAL * nuevoTotal * 1000;
+	int ticketsProceso = tickets/nuevoTotal;
+
+	for (i = 0; i < nuevoTotal; i++) {
+		processTickets[i] = ticketsProceso;
+	}
+}
+
+int SchedLottery::next(int cpu) {
+	int ticketGanador, procesoGanador, suma = -1, length = processTickets.size();
+	
+	ticketGanador = rand()%(tickets + 1);
+	quantumActual[cpu] = QUANTUM_TOTAL;
+
+	for (int i = 0; i < length; i++) {
+		suma += processTickets[i];
+
+		if (suma <= ticketGanador) {
+			procesoGanador = process[i];
+			break;
+		}
+	}
+	
+	// lo saco de la lista de procesos disponibles
+	sacar(procesoGanador);
+	return procesoGanador;
 }
--- entregable/codigo/sched_lottery.h
+++ entregable/codigo/sched_lottery.h
@@ -7,12 +7,20 @@
 class SchedLottery : public SchedBase {
   public:
     SchedLottery(std::vector<int> argn);
-        ~SchedLottery();
+    ~SchedLottery();
     virtual void load(int pid);
     virtual void load(int pid, int deadline);
     virtual void unblock(int pid);
     virtual int tick(int cpu, const enum Motivo m);
 
+private:
+	std::vector<int> quantumActual;
+	std::vector<int> process;
+	std::vector<int> processTickets;
+	int semilla;
+	int tickets;
+	void sacar(int pid);
+	int next(int cpu);
 };
 
 #endif
